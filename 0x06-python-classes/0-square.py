0eb56058e51410333b94078bc07d6bbed616da55
owolabi250 <owolabikehinde250@gmail.com>
2022-09-14 08:58:45 +0000

    good

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+"""This module contains a class called Square that defines as module contains a class called Square that defines a square."""
+
+Class Square: 
+	"""This is the square class definition."""
+	pass
