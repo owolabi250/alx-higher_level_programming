2c382a387ecb40a0a3f570e0af04a84932064ebc
root <root@DESKTOP-7MAUJJU.localdomain>
2022-09-21 10:17:45 +0100

    good

--- 0x07-python-test_driven_development/kk.py
+++ 0x07-python-test_driven_development/kk.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
-print(matrix)
+print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
